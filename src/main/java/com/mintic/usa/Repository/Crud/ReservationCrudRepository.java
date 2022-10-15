d1eccc109406aa5987a9dae1744fcb382f48db0b
RubVic36 <victor.rubio.mt@usa.edu.co>
2022-10-15 11:45:03 -0500

    mi primer commit

---
+++ src/main/java/com/mintic/usa/Repository/Crud/ReservationCrudRepository.java
@@ -0,0 +1,7 @@
+package com.mintic.usa.Repository.Crud;
+
+import com.mintic.usa.Modelo.Reservation;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReservationCrudRepository extends CrudRepository<Reservation, Integer> {
+}
